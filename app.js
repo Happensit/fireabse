81e2901b185860e5b32f1e08c4d0bd18269349ae
Happensit <antonybizov@gmail.com>
2017-07-04 18:26:11 +0300

    Token required

--- app.js
+++ app.js
@@ -182,7 +182,7 @@ function getToken() {
 
 
 function sendNotification(notification) {
-    var key = 'AAAAaGQ_q2M:APA91bGCEOduj8HM6gP24w2LEnesqM2zkL_qx2PJUSBjjeGSdJhCrDoJf_WbT7wpQZrynHlESAoZ1VHX9Nro6W_tqpJ3Aw-A292SVe_4Ho7tJQCQxSezDCoJsnqXjoaouMYIwr34vZTs';
+    var key = 'AAAAgSXESYs:APA91bFvzVHHHgKfvnlV_g3zABQ1yqsGiN5ciWoApe4fvRZhE5e_sscsuDuY-dgutN3TQ8tNo8DHxccRadiS3PmYHw1rqujhpy5Ogodfc4pWj3jk0c1Bqgl7RF85PqqvKQo_ptS-VfHX';
 
     console.log('Send notification', notification);
 
