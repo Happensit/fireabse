e2f3f9cf0e80628ba4e52fadd795c92d98a27edb
Happensit <antonybizov@gmail.com>
2018-07-05 11:11:00 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
 
 firebase.initializeApp({
-    'messagingSenderId': '554684402059'
+    'messagingSenderId': '83536372814'
 });
 
 firebase.messaging();
